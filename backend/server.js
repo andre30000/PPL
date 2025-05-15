02a38002b66be30b54ea964d064e5bb46ac6edd1
andre30000 <120888724+andre30000@users.noreply.github.com>
2025-05-15 15:00:03 +0000

    backend fix

--- backend/server.js
+++ backend/server.js
@@ -88,14 +88,14 @@ app.delete('/api/workouts/:id', async (req, res) => {
   }
 });
 
-// Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+// Serve static assets in production - only needed in the backend is serving the front end - REMOVED
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static('client/build'));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   });
+// }
 
 // Set port
 const PORT = process.env.PORT || 5000;
